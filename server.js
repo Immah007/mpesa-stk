bf8a5e9d951efcda590088f766dfc231791e66dc
Immah007 <112688332+Immah007@users.noreply.github.com>
2026-02-19 23:23:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -90,10 +90,10 @@ app.post('/stkpush', async (req, res) => {
       BusinessShortCode:  SHORTCODE,
       Password: password,
       Timestamp: timestamp,
-      TransactionType: 'CustomerBuyGoodsOnline', // 'CustomerPayBillOnline',
+      TransactionType: 'CustomerPayBillOnline', // 'CustomerBuyGoodsOnline', // 'CustomerPayBillOnline',
       Amount: amount,
       PartyA: phone,
-      PartyB: '4959216',// SHORTCODE, 5467496
+      PartyB: '5467496', // '4959216',// SHORTCODE, 5467496
       PhoneNumber: phone,
       CallBackURL: CALLBACK_URL,
       AccountReference: 'Merchant Store here',
@@ -351,6 +351,7 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
+
 
 
 
